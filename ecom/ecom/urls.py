d7c5ed185b663e0e4e116f4fac11c96618e446da
kamlesh502 <kamleshbishnoi502@gmail.com>
2020-10-08 14:34:43 +0530

    After creating the user with their Unique token and session id Created the order app which consist all the info regrading the order

--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -18,10 +18,16 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 
+def home(request):
+    return render(request,)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/',include('api.urls'))
+    path('api/',include('api.urls')),
+
+    
+
 
 ]
 
